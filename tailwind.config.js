f4c049c39a54e95bfaa02ab0264e183e1ef86960
Pablo <cv.juanp@gmail.com>
2023-07-30 10:38:13 -0700

    ADD - PictureBlob component / animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      animation: {
+        blobMorph: "blobMorph 7s ease-in-out infinite",
+      },
+      keyframes: {
+        blobMorph: {
+          "0,100%": { "border-radius": "60% 40% 30% 70%/60% 30% 70% 40%" },
+          "50%": { " border-radius": "40% 70% 70% 60%/50% 60% 30% 60%" },
+        },
       },
     },
   },
   plugins: [],
-}
+};
