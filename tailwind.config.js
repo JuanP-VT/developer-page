340d5f402cf822995a2b17aa2ae24c203f040c74
PB-VT <98140708+JuanP-VT@users.noreply.github.com>
2023-07-31 21:06:31 -0700

    UPD - Add custom width

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      width: {
+        "128":"32rem"
+      },
       animation: {
         blobMorph: "blobMorph 7s ease-in-out infinite",
       },
