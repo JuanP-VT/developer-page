d204db84591dbecccfdec5d7a1fb71daf560933b
PB-VT <98140708+JuanP-VT@users.noreply.github.com>
2023-08-03 14:49:03 -0700

    UPD - IESkill component

--- app/components/About me/IESkill.tsx
+++ app/components/About me/IESkill.tsx
@@ -4,10 +4,16 @@ type Props = { name: string; description: string; imageSrc: string };
 
 function IESkill({ name, description, imageSrc }: Props) {
   return (
-    <div className="relative flex w-72 flex-col items-center">
+    <div className="relative flex w-72 flex-col items-center self-center">
       <Image src={imageSrc} alt="skill" width={100} height={100} className="" />
       <div className="">
-        <div data-description={description} className="IESkill flex p-2">
+        <div
+          data-description={description}
+          className={`inline-block cursor-pointer p-2 text-center after:absolute 
+          after:bottom-0 after:left-0 after:hidden after:w-72 after:rounded-lg after:border-2 
+          after:bg-black after:p-5 after:text-13px after:text-white  after:transition-all 
+          after:content-[attr(data-description)] hover:after:block`}
+        >
           {name}
         </div>
       </div>
