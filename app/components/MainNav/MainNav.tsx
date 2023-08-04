f99511bc04079853f848f90125ee3f244dd1bca1
PB-VT <98140708+JuanP-VT@users.noreply.github.com>
2023-08-03 22:48:39 -0700

    ADD - Scroll effect

--- app/components/MainNav/MainNav.tsx
+++ app/components/MainNav/MainNav.tsx
@@ -1,12 +1,37 @@
-import React from "react";
+/**
+ * Represents the main navigation bar of the application.
+ *
+ * This component displays a fixed navigation bar at the top of the page.
+ * It contains links to different sections of the application, such as Home, Tech Stack, Projects, and About Me.
+ * The navigation bar's appearance changes based on the scroll position:
+ * - When the user scrolls down more than 100 pixels, the background turns black.
+ * - Otherwise, the background transitions from a slate color to the default background color.
+ *
+ * @component
+ *
+ * @returns {JSX.Element} JSX element representing the main navigation bar of the application.
+ */
+import { useEffect, useState } from "react";
 
-type Props = {};
-
-function MainNav({}: Props) {
+function MainNav() {
+  const [currentY, setCurrentY] = useState(0);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  const handleScroll = () => {
+    setTimeout(() => {
+      setCurrentY(window.scrollY);
+    }, 200);
+  };
   return (
     <div
-      className="fixed top-0 z-20 flex w-full justify-center gap-3 
-     rounded-b-lg bg-black p-2 text-xs sm:justify-end sm:text-base"
+      className={`fixed top-0 z-20 flex w-full justify-center gap-3 
+      rounded-b-lg ${
+        currentY > 100 ? "bg-slate-700" : "bg-black transition-all duration-300"
+      } p-2 text-xs sm:justify-end sm:text-base`}
     >
       <a href="#Home" className="font-medium text-white hover:text-sky-300">
         Home
