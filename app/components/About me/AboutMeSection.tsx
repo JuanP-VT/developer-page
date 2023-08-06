e577960d68909f7de199ff7af61eec1118a8dfde
PB-VT <98140708+JuanP-VT@users.noreply.github.com>
2023-08-06 11:58:28 -0700

    UPD - add contact

--- app/components/About me/AboutMeSection.tsx
+++ app/components/About me/AboutMeSection.tsx
@@ -8,22 +8,54 @@
  * @component
  * @returns {JSX.Element} JSX element representing the About Me section.
  */
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import Hobbies from "./Hobbies";
 import AboutMeNav from "./AboutMeNav";
 import Background from "./Background";
+import { faMailBulk, faMapLocation } from "@fortawesome/free-solid-svg-icons";
 
 function AboutMeSection() {
   const [currentSection, setCurrentSection] = useState<
     "hobbies" | "background"
   >("hobbies");
   return (
-    <section id="AboutMe" className="flex-w relative bg-slate-300 pt-20">
+    <section id="AboutMe" className="flex-w relative  bg-slate-300 pt-20">
       <AboutMeNav
         setCurrentSection={setCurrentSection}
         currentSection={currentSection}
       />
       {currentSection === "hobbies" ? <Hobbies /> : <Background />}
+      <div className="flex-col p-5">
+        <p className="mb-3 text-xl font-bold text-blue-600">Contact</p>
+        <div className=" flex flex-col gap-5 sm:flex-row">
+          <div className="flex gap-5">
+            <div className="flex items-center">
+              <FontAwesomeIcon
+                className=" h-10 w-10 text-blue-500"
+                icon={faMapLocation}
+              />
+            </div>
+            <div className="flex flex-col font-medium">
+              <p className="font-semibold">Location</p>
+              <p>México, Juan José Ríos</p>
+            </div>
+          </div>
+          <div className="flex gap-5">
+            <div className="flex items-center">
+              <FontAwesomeIcon
+                className=" h-10 w-10 text-blue-500"
+                icon={faMailBulk}
+              />
+            </div>
+            <div className="flex flex-col font-medium">
+              <p className="font-semibold">Mail</p>
+              <a href="mailto:cv.juanp@gmail.com">cv.juanp@gmail.com</a>
+            </div>
+          </div>
+          <div className="flex"></div>
+        </div>
+      </div>
     </section>
   );
 }
